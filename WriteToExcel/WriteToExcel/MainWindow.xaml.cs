e2baf2b3f279467ba3921256a55b427ddddcc4e8
IskrenOff <iskren.mirev@gmail.com>
2023-11-11 20:29:46 +0200

    commit

--- WriteToExcel/WriteToExcel/MainWindow.xaml.cs
+++ WriteToExcel/WriteToExcel/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WriteToExcel
         {
             InitializeComponent();
         }
+        
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
     }
 }
