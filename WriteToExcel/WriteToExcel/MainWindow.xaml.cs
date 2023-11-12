fb612b60cb7ea1b6def0d5d3263c226b59107d74
IskrenOff <iskren.mirev@gmail.com>
2023-11-12 19:51:36 +0200

    adding control bar button and functionality

--- WriteToExcel/WriteToExcel/MainWindow.xaml.cs
+++ WriteToExcel/WriteToExcel/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Runtime;
+using System.Windows.Forms;
+using System.DirectoryServices;
+using System.Windows.Interop;
 
 namespace WriteToExcel
 {
@@ -22,8 +27,11 @@ namespace WriteToExcel
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();         
         }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage (IntPtr nWnd, int wMsg, int wParam, int lParam);
         
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -31,7 +39,35 @@ namespace WriteToExcel
 
         private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            DragMove();
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState== WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState= WindowState.Normal;
+            }
+        }
+
+        private void pnlControlBar_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
     }
 }
