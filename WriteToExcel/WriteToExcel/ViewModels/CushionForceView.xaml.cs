8cee13ba2769fa0ed4c0f8efb43c444e6696de69
IskrenOff <iskren.mirev@gmail.com>
2023-12-10 13:53:53 +0200

    All crosshairs are added to all the views

--- WriteToExcel/WriteToExcel/ViewModels/CushionForceView.xaml.cs
+++ WriteToExcel/WriteToExcel/ViewModels/CushionForceView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScottPlot.Plottable;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -55,18 +56,7 @@ namespace WriteToExcel.ViewModels
                 CushionForce.Plot.AddScatter(dataX, dataY, color: System.Drawing.Color.Magenta);
                 CushionForce.Render();
 
-
-                //Adding crosshaid lines
-                var crosshairVertical = CushionForce.Plot.AddVerticalLine(0);
-                var crosshairHorizontal = CushionForce.Plot.AddHorizontalLine(0);
-
-                //Color of the crosshair
-                crosshairVertical.Color = System.Drawing.Color.Red;
-                crosshairHorizontal.Color = System.Drawing.Color.Red;
-
-                // Create text objects to display crosshair position
-                var labelVertical = CushionForce.Plot.AddText("0", x: 0, y: 0, color: System.Drawing.Color.Yellow);
-                var labelHorizontal = CushionForce.Plot.AddText("0", x: 0, y: 0, color: System.Drawing.Color.Yellow);
+                Crosshair cross = CushionForce.Plot.AddCrosshair(25, .5);
 
                 // Subscribe to the MouseMoved event to update the crosshair position
                 CushionForce.MouseMove += (s, e) =>
@@ -75,22 +65,9 @@ namespace WriteToExcel.ViewModels
                     (double mouseX, double mouseY) = CushionForce.GetMouseCoordinates();
 
                     // Update the crosshair position with the mouse coordinates
-                    crosshairVertical.X = mouseX;
-                    crosshairHorizontal.Y = mouseY;
+                    cross.X = mouseX;
+                    cross.Y = mouseY;
 
-
-
-                    // Update the labels to display crosshair position
-                    labelVertical.X = mouseX;
-                    labelVertical.Y = mouseY - 500; // adjust label position
-                    labelVertical.Label = mouseX.ToString("F2");
-
-                    labelHorizontal.Y = mouseY;
-                    labelHorizontal.X = mouseX + 5; // adjust label position
-                    labelHorizontal.Label = mouseY.ToString("F2");
-
-
-                    // Redraw the plot
                     CushionForce.Render();
                 };
             }
--- WriteToExcel/WriteToExcel/ViewModels/CushionPositionView.xaml.cs
+++ WriteToExcel/WriteToExcel/ViewModels/CushionPositionView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScottPlot.Plottable;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -55,18 +56,7 @@ namespace WriteToExcel.ViewModels
                 CushionPosition.Plot.AddScatter(dataX, dataY, color: System.Drawing.Color.Orange);
                 CushionPosition.Render();
 
-
-                //Adding crosshaid lines
-                var crosshairVertical = CushionPosition.Plot.AddVerticalLine(0);
-                var crosshairHorizontal = CushionPosition.Plot.AddHorizontalLine(0);
-
-                //Color of the crosshair
-                crosshairVertical.Color = System.Drawing.Color.Red;
-                crosshairHorizontal.Color = System.Drawing.Color.Red;
-
-                // Create text objects to display crosshair position
-                var labelVertical = CushionPosition.Plot.AddText("0", x: 0, y: 0, color: System.Drawing.Color.Yellow);
-                var labelHorizontal = CushionPosition.Plot.AddText("0", x: 0, y: 0, color: System.Drawing.Color.Yellow);
+                Crosshair cross = CushionPosition.Plot.AddCrosshair(25, .5);
 
                 // Subscribe to the MouseMoved event to update the crosshair position
                 CushionPosition.MouseMove += (s, e) =>
@@ -75,22 +65,9 @@ namespace WriteToExcel.ViewModels
                     (double mouseX, double mouseY) = CushionPosition.GetMouseCoordinates();
 
                     // Update the crosshair position with the mouse coordinates
-                    crosshairVertical.X = mouseX;
-                    crosshairHorizontal.Y = mouseY;
+                    cross.X = mouseX;
+                    cross.Y = mouseY;
 
-
-
-                    // Update the labels to display crosshair position
-                    labelVertical.X = mouseX;
-                    labelVertical.Y = mouseY - 500; // adjust label position
-                    labelVertical.Label = mouseX.ToString("F2");
-
-                    labelHorizontal.Y = mouseY;
-                    labelHorizontal.X = mouseX + 5; // adjust label position
-                    labelHorizontal.Label = mouseY.ToString("F2");
-
-
-                    // Redraw the plot
                     CushionPosition.Render();
                 };
             }
