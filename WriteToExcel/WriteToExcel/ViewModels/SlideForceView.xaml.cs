b7fe979a038bf5a49bafb6e99b1860417c2c4ac3
IskrenOff <iskren.mirev@gmail.com>
2023-12-10 13:49:13 +0200

    Adding functional crosshair , follows the mouse courser

--- WriteToExcel/WriteToExcel/ViewModels/SlideForceView.xaml.cs
+++ WriteToExcel/WriteToExcel/ViewModels/SlideForceView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using FontAwesome.Sharp;
 using Microsoft.VisualBasic.Devices;
 using ScottPlot;
+using ScottPlot.MarkerShapes;
 using ScottPlot.Plottable;
 using System;
 using System.Collections.Generic;
@@ -64,42 +65,15 @@ namespace WriteToExcel.ViewModels
                 SlideForce.Plot.AddScatter(dataX, dataY, color: System.Drawing.Color.Aquamarine);
                 SlideForce.Render();
 
-
-                //Adding crosshaid lines
-                var crosshairVertical = SlideForce.Plot.AddVerticalLine(0);
-                var crosshairHorizontal = SlideForce.Plot.AddHorizontalLine(0);
-
-                //Color of the crosshair
-                crosshairVertical.Color = System.Drawing.Color.Red;
-                crosshairHorizontal.Color = System.Drawing.Color.Red;
-
-                // Create text objects to display crosshair position
-                var labelVertical = SlideForce.Plot.AddText("0", x: 0, y: 0, color: System.Drawing.Color.Yellow);
-                var labelHorizontal = SlideForce.Plot.AddText("0", x: 0, y: 0, color: System.Drawing.Color.Yellow);
-
-                // Subscribe to the MouseMoved event to update the crosshair position
+                Crosshair cross = SlideForce.Plot.AddCrosshair(25, .5);
+ 
+                // Subscribe to the MouseMoved event to update the Crosshair position
                 SlideForce.MouseMove += (s, e) =>
                 {
-                    // Get the mouse coordinates in plot space
                     (double mouseX, double mouseY) = SlideForce.GetMouseCoordinates();
+                    cross.X = mouseX;
+                    cross.Y = mouseY;
 
-                    // Update the crosshair position with the mouse coordinates
-                    crosshairVertical.X = mouseX;
-                    crosshairHorizontal.Y = mouseY;
-
-
-
-                    // Update the labels to display crosshair position
-                    labelVertical.X = mouseX;
-                    labelVertical.Y = mouseY - 500; // adjust label position
-                    labelVertical.Label = mouseX.ToString("F2");
-
-                    labelHorizontal.Y = mouseY;
-                    labelHorizontal.X = mouseX + 5; // adjust label position
-                    labelHorizontal.Label = mouseY.ToString("F2");
-
-
-                    // Redraw the plot
                     SlideForce.Render();
                 };
             }
